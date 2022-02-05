52e6414d32f1339afe735845759b443e7293a9b5
i-am-VasuKatravath <katravathvasu000@iitkgp.ac.in>
2022-02-05 12:52:20 +0530

    Update Bookshop Automation Software.py

--- Bookshop Automation Software.py
+++ Bookshop Automation Software.py
@@ -1,22 +1,22 @@
 print("BOOKSHOP AUTOMATION SYSTEM")
 def open_Bookshop():
     print("Welcome to the new version of Bookshop :)")
-    print(">Open or close")
+    print("> Open or close")
     is_type=False
     while not is_type:
         try:
-            open_close=(input(">Type 'Yes' to open and 'No' to close: ").lower())
+            open_close=(input("> Type 'Yes' to open and 'No' to close: ").lower())
             if(open_close=="yes"):
-                print(">Let's get started")
+                print("> Let's get started")
                 break
             elif(open_close=="no"):
-                print(">Thanks-Visit Again")
+                print("> Thanks-Visit Again")
                 exit()
         except Exception:
-            print(">Wrong!-Type/Input correctly")  
+            print("> Wrong!-Type/Input correctly")  
             is_type=False
         else:
-            print(">only choose 'Yes' or 'No'")
+            print("> only choose 'Yes' or 'No'")
             is_type=False
         
 open_Bookshop()            
