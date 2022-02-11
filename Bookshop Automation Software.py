56dba50dfbdb97cf3f762d2357873d2aa23c96b9
i-am-VasuKatravath <katravathvasu000@iitkgp.ac.in>
2022-02-11 11:23:59 +0530

    Update Bookshop Automation Software.py
    Now using Tkinter- added buttons and entry widget

--- Bookshop Automation Software.py
+++ Bookshop Automation Software.py
@@ -9,7 +9,67 @@ BAS will help the manager to periodically view the request field of the books so
 rough estimate regarding the current demand for different books. Also it maintains the price of
 various books.
 '''
-print("BOOKSHOP AUTOMATION SYSTEM")
+
+
+from tkinter import *
+from tkinter import font
+root=Tk()
+
+def main_window():
+    root.title("BOOKSHOP AUTOMATION SYSTEM")
+    software_name=Label(root, text="Welcome to the new version of Bookshop :)", padx=20, pady=40, fg="red", bg="yellow")
+    software_name.grid(row=0,column=0,columnspan=2)
+
+#software_name.pack() // cannot be used both grid and pack at once.
+def open():
+    
+    open_label=Label(root, text="Check for availabilty", padx=100, pady=20, bg="Orange")
+    open_label.grid(row=2,column=0,columnspan=2)
+    input_name=Entry(root, text="Enter Author name or title of the book", width=40, borderwidth=10)
+    input_name.grid(row=3,columnspan=2)
+
+def clear_button():
+    root2.destroy()
+    
+
+def close():
+    root.destroy()
+    global root2
+    root2=Tk()
+    root2.title("BOOKSHOP AUTOMATION SYSTEM")
+    
+    close_label=Label(root2, text="Thanks!, visit again:) ",bg="red", padx=50,pady=50)
+    close_label.pack()
+    button_clear=Button(root2, text="Clear! ", bg="Blue", padx=50, pady=25, bd=10, command=clear_button)
+    button_clear.pack()
+    
+    
+    
+def button():   
+    
+    open_button=Button(root, text="open",bd=10, padx=50, pady=25, command=open, bg="green")
+    close_button=Button(root, text="close",bd=10, padx=50, pady=25, command=close, bg="red")
+    open_button.grid(row=1,column=0)
+    close_button.grid(row=1,column=1)
+
+
+
+
+def user():
+    
+    main_window()
+    button()
+def next_user():
+    global root2
+    root2.destroy()
+    user()
+start=True
+
+user()
+root.mainloop()
+
+
+'''
 def open_Bookshop():
     print("Welcome to the new version of Bookshop :)")
     print("> Open or close")
@@ -31,14 +91,16 @@ def open_Bookshop():
             is_type=False
         
 open_Bookshop() 
-books_database=[]      
+books_database=[]    
+'''  
 '''     
 user interface contains many options to be selected by the user
 
-'''
+
 def availability_of_book():
     check_availability=input("check for availability of boook ").lower()
     if(check_availability=="yes"):
         print("Enter the key words i.e., Book Title or name of the author")
 
 #learn sql-Database Management System.
+'''
